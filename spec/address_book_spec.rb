cf97f85138f45174a3697f54befaa1932876deb9
Harold Asai <koichiroasai@gmail.com>
2017-05-15 10:03:38 -0700

    address_bloc-assignment4-completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,16 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
-end
+
+  describe "#remove_entry" do
+  	it "remove one entry from the address book" do
+  		book = AddressBook.new
+  		book.add_entry('Harold Asai', '010.012.1814', 'muhimuhi.king@asai.com')
+  		book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+  		expect(book.entries.size).to eq(2)
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq("Harold Asai")
+    end  
+  end
+end  
